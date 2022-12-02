8ca9e81cceccb4f424e6b2a6a8417836d6130348
adolfoholic <adolfo.rd32@gmail.com>
2022-12-01 20:46:01 -0500

    Actualizacion de archivos en repositorio personal

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,39 @@
+const {DataTypes} = require('sequelize')
 
+const db = require('../utils/database')
+
+const Users = db.define('users', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
+    }
+}, {
+    timestamps: false //No agrega las columnas created_at y updated_at
+})
+
+module.exports = Users
 
 
 
